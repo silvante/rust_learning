039a3fe5e2e0d168f8044cc3274b44e54e9e63f6
silvante <khamidov.ko@gmail.com>
2025-12-15 19:49:44 +0500

    Well not bad now

--- cargo/types/src/main.rs
+++ cargo/types/src/main.rs
@@ -2,9 +2,9 @@
 // arrays, tuples, slices, and strings (sliced string)
 
 fn main() {
-    let ints: [i32; 4] = [82, 37, 28, 45];
-    println!("Array of numbers: {:?}", ints);
+    // let ints: [i32; 4] = [82, 37, 28, 45];
+    // println!("Array of numbers: {:?}", ints);
 
     let strs: [&str; 3] = ["react", "vue", "svalte"];
-    println!("Array of strings: {:?}", strs)
+    println!("My fav front end framework is: {}", strs[0])
 }
