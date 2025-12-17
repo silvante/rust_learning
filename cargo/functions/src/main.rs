63e98b1ef0014c1213aed00eb71760754b14f66b
silvante <khamidov.ko@gmail.com>
2025-12-18 00:51:45 +0500

    structs

--- cargo/functions/src/main.rs
+++ cargo/functions/src/main.rs
@@ -1,10 +1,14 @@
 // so for some reason all names of functions and vars should be in snake case (I like it)
 fn main() {
-    let result: i32 = add(48, 82);
+    let result: i32 = JustStruct::add(48, 82);
 
     println!("result is: {}", result)
 }
 
-fn add(a: i32, b: i32) -> i32 {
-    return a + b;
+struct JustStruct {}
+
+impl JustStruct {
+    fn add(a: i32, b: i32) -> i32 {
+        return a + b
+    }
 }
