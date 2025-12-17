9f9fac1769e857fc5e78c66c82cfa5407ae4d620
silvante <khamidov.ko@gmail.com>
2025-12-17 18:40:20 +0500

    Well not bad now

---
+++ cargo/functions/src/main.rs
@@ -0,0 +1,4 @@
+// so for some reason all names of functions and vars should be in snake case (I like it)
+fn main() {
+    println!("Hello, world!");
+}
---
+++ cargo/functions/target/debug/deps/functions-e3aa52be5f13d904.d
@@ -0,0 +1,5 @@
+/home/valentine/products/rust/cargo/functions/target/debug/deps/functions-e3aa52be5f13d904.d: src/main.rs
+
+/home/valentine/products/rust/cargo/functions/target/debug/deps/libfunctions-e3aa52be5f13d904.rmeta: src/main.rs
+
+src/main.rs:
