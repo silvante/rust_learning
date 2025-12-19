6e40d3e7eaaa0d64e2ec126e4a46a0796afa79fa
silvante <khamidov.ko@gmail.com>
2025-12-19 15:56:48 +0500

    just code

--- cargo/functions/src/main.rs
+++ cargo/functions/src/main.rs
@@ -5,6 +5,12 @@ fn main() {
     println!("result is: {}", result);
 
     println!("function add returned, {}. ", JustStruct::add(37, 82));
+
+    let weight: f32 = 77.0;
+    let height: f32 = 1.84;
+
+    let bmi: f32 = JustStruct::col_bmi(weight, height);
+    println!("Your BMI is: {:.2}", bmi);
 }
 
 struct JustStruct {}
@@ -12,5 +18,11 @@ struct JustStruct {}
 impl JustStruct {
     fn add(a: i32, b: i32) -> i32 {
         return a + b
+    }
+}
+
+impl JustStruct {
+    fn col_bmi(kg: f32, hm: f32) -> f32 {
+        return kg / (hm * hm)
     }
 }
