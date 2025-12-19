cdcbba8d50fb494b22a06d3cdeba9175404c142f
silvante <khamidov.ko@gmail.com>
2025-12-19 20:48:42 +0500

    ownership is not that hard

--- cargo/ownership/src/main.rs
+++ cargo/ownership/src/main.rs
@@ -1,10 +1,17 @@
+// fn main() {
+//    let s1 = String::from("RUST");
+//    let len = calculate_length(&s1); 
+//
+//    println!("length of {} is {}", s1, len);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//    return s.len()
+// }
+
 fn main() {
-    let s1 = String::from("RUST");
-    let len = calculate_length(&s1); 
+    let owner1 = String::from("SingleValue");
+    let owner2 = owner1;
 
-    println!("length of {} is {}", s1, len);
-}
-
-fn calculate_length(s: &String) -> usize {
-    return s.len()
+    println!("value is {}", owner2); // error
 }
