cf8a97c8cbc66fe93dd47440814213448afa1ee8
silvante <khamidov.ko@gmail.com>
2025-12-24 19:23:10 +0500

    code

--- cargo/loops/src/main.rs
+++ cargo/loops/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let mut counter: i32 = 0;
-
+    // let mut counter: i32 = 0;
     // let result: i32 = loop {
     //     counter += 1;
     //     println!("counting {counter}");
@@ -10,12 +9,14 @@ fn main() {
     //         break counter * 2;
     //     };
     // };
-
+    
     // println!("result out of loop is {result}")
+    
+    let mut counter: i32 = 0;
 
     'counting_up: loop {
         println!("counter = {counter}");
-        let mut remaining = 10;
+        let mut remaining: i32 = 10;
 
         loop {
             println!("remaining = {remaining}");
@@ -26,6 +27,13 @@ fn main() {
             if counter == 2 {
                 break 'counting_up;
             }
+            remaining -= 1;
         }
+        counter += 1;
+    }
+
+    let arr = [1, 2, 3, 4, 5, 6, 7];
+    for element in arr {
+        println!("in arr: {}", element)
     }
 }
