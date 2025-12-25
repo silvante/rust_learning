76e81533bd7e3917a7e63f40c6b6f00eb219d7d9
silvante <khamidov.ko@gmail.com>
2025-12-25 23:54:02 +0500

    I got structs

--- cargo/structs/src/main.rs
+++ cargo/structs/src/main.rs
@@ -15,11 +15,35 @@ fn main() {
         "khamidov.ko@gmail.com".to_string(),
     );
 
+    println!("\n> User1");
     println!("user is active? => {}", user.active);
     println!("user is => {}", user.name);
     println!("his email is => {}", user.email);
     println!("his username is {}", user.username);
     println!("user is => {} years old", user.age);
+
+    let user2 = User {
+        email: "info@xamidov.uz".to_string(),
+        username: "xamidov".to_string(),
+        name: "Mardonbek Xamidov".to_string(),
+        ..user // it is real fun here :)
+    };
+    // so in this example we didn't use any ::String fields of "user" (user1),
+    // so name, username, and email was not moved to user2,
+    // means we can use user after declining user2,
+    // we also can use fiels like active and age after user2 becouse they are both not strings and they didn't move too.
+
+    println!("\n> User2");
+    println!("user is active? => {}", user2.active);
+    println!("user is => {}", user2.name);
+    println!("his email is => {}", user2.email);
+    println!("his username is {}", user2.username);
+    println!("user is => {} years old", user2.age); // not bad I can understand it
+
+    // using user1 here
+    // cuz we didn't move any String fields to user2, super
+    
+    println!("\nuser1 is avaible tho :) => {}, {}, {}", user.active, user.email, user.username);
 }
 
 fn build_user(name: String, email: String) -> User {
@@ -27,9 +51,9 @@ fn build_user(name: String, email: String) -> User {
 
     User {
         active: true,
-        name: name,
-        username: username,
-        email: email,
+        name,
+        username,
+        email,
         age: 18,
     }
 }
