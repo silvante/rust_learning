66776285b1bcc53daaee72d16d2a593c5c996d31
silvante <khamidov.ko@gmail.com>
2025-12-25 23:28:12 +0500

    structs as expected was really fun to use

--- cargo/structs/src/main.rs
+++ cargo/structs/src/main.rs
@@ -1,13 +1,37 @@
 fn main() {
-    let user: User = User {
-        active: true,
-        name: String::from("Mardonbek Khamidov"),
-        username: String::from("xamidov"),
-        email: String::from("khamidov.ko@gmail.com"),
-        age: 17
-    };
+    // let mut user: User = User {
+    //     active: true,
+    //     name: String::from("Mardonbek Khamidov"),
+    //     username: String::from("xamidov"),
+    //     email: String::from("khamidov.ko@gmail.com"),
+    //     age: 17
+    // };
 
-    println!("user is {}", user.name);
+    // println!("his email was {}", user.email);
+
+    // user.email = String::from("info@xamidov.uz");
+    let user = build_user(
+        "Mardonbek Khamidov".to_string(),
+        "khamidov.ko@gmail.com".to_string(),
+    );
+
+    println!("user is active? => {}", user.active);
+    println!("user is => {}", user.name);
+    println!("his email is => {}", user.email);
+    println!("his username is {}", user.username);
+    println!("user is => {} years old", user.age);
+}
+
+fn build_user(name: String, email: String) -> User {
+    let username = name.trim().to_lowercase().replace(" ", "");
+
+    User {
+        active: true,
+        name: name,
+        username: username,
+        email: email,
+        age: 18,
+    }
 }
 
 struct User {
