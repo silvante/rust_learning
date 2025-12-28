08e7496412721717e1de811c74d0c1ede2ccf052
silvante <khamidov.ko@gmail.com>
2025-12-28 20:10:06 +0500

    easy to understand but IDK where to use

--- cargo/enums/src/main.rs
+++ cargo/enums/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let four: IPsKind = IPsKind::V4("127.0.0.1".to_string());
+    let four: IPsKind = IPsKind::V4(127, 0, 0, 1);
     let six: IPsKind = IPsKind::V6("::1".to_string());
 
     println!("Hello, World!")
 }
 
 enum IPsKind {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
