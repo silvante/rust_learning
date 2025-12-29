356aeccd33ce4af702cb2ad5cec5cfa72995d3b6
silvante <khamidov.ko@gmail.com>
2025-12-29 22:06:59 +0500

    I do not have an idea

---
+++ cargo/option/target/debug/deps/option-f27b42fdf9fcf191.d
@@ -0,0 +1,5 @@
+/home/xamidov/devs/rust_learning/cargo/option/target/debug/deps/option-f27b42fdf9fcf191.d: src/main.rs
+
+/home/xamidov/devs/rust_learning/cargo/option/target/debug/deps/option-f27b42fdf9fcf191: src/main.rs
+
+src/main.rs:
---
+++ cargo/option/target/debug/option.d
@@ -0,0 +1,1 @@
+/home/xamidov/devs/rust_learning/cargo/option/target/debug/option: /home/xamidov/devs/rust_learning/cargo/option/src/main.rs
